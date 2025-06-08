83c181a8e92fbaad7c26e0b6e1833f0c74c98af4
Dominik Breksa <dominikbreksa@gmail.com>
2025-06-08 19:20:56 +0200

    Removed the math module from monte_carlo.py

--- PySDM/physics/particle_advection/monte_carlo.py
+++ PySDM/physics/particle_advection/monte_carlo.py
@@ -8,7 +8,7 @@ class MonteCarlo:
     def displacement(_, position_in_cell, c_l, c_r, u01):
         c = max(c_l, c_r)
         probability_of_shift = abs(c)
-        guaranteed_shift = math.floor(probability_of_shift)
+        guaranteed_shift = int(probability_of_shift // 1)
         probability_of_shift -= guaranteed_shift
 
         sign = int(abs(c) / c)
